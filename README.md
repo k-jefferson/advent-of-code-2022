6e6d9f43dbf15f681eadb9aedef463a37ea008db
Nicolas Labey <nicolas@weda.fr>
2022-12-01 11:01:19 +0100

    day 1

---
+++ README.md
@@ -0,0 +1,11 @@
+# adventofcode
+adventofcode.com 2022 santa event
+
+to transpile TypeScript to JavaScript:
+tsc
+
+to run the app:
+node dist/src/app
+
+to run the unit tests:
+npx mocha dist/tests/test.js
---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import * as day1 from "./day1.js";
+
+console.log("**** Advent of Code ****");
+console.log("=> day 1");
+console.log("part 1");
+console.log(day1.part1());
+console.log("part 2");
+console.log(day1.part2());
